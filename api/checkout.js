9f7b69c7f4c286f89888aed8ad19cdadb244cb38
Fooutte <78595822+Fooutte@users.noreply.github.com>
2025-12-18 23:49:18 -0500

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -1,11 +1,6 @@
 const nodemailer = require('nodemailer');
 
 export default async function handler(req, res) {
-    // Autoriser ton site à parler à ce serveur
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
     if (req.method === 'OPTIONS') return res.status(200).end();
 
     const { text, color, size, customerName, customerEmail } = req.body;
@@ -22,10 +17,10 @@ export default async function handler(req, res) {
         await transporter.sendMail({
             from: 'jerome.vaillancourt200@gmail.com',
             to: 'jerome.vaillancourt200@gmail.com',
-            subject: `COMMANDE DE : ${customerName}`,
-            text: `Détails:\nNom: ${customerName}\nEmail: ${customerEmail}\nTexte: ${text}\nCouleur: ${color}\nTaille: ${size}`
+            subject: `COMMANDE : ${customerName}`,
+            text: `Nouvelle commande !\n\nClient: ${customerName}\nEmail: ${customerEmail}\nDesign: ${text}\nCouleur: ${color}\nTaille: ${size}`
         });
-        return res.status(200).json({ message: "Envoyé !" });
+        return res.status(200).json({ message: "Envoyé" });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
